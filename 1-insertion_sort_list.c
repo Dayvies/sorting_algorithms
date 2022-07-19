074adf89520805b681cb0704c9a0f06a1406aeaf
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-07-19 19:03:24 +0300

    Sort Debug

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,16 +1,16 @@
 #include "sort.h"
+int len_list(listint_t *h);
 /**
  * insertion_sort_list - insert sort list
  * @list: list
  * Returns: void
  */
-
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *small, *big;
 
 	current = *list;
-	if (!list || !(*list) || current->next == NULL)
+	if (!list || !(*list) || len_list(*list) < 2)
 		return;
 	while (current)
 	{
@@ -42,4 +42,21 @@ void insertion_sort_list(listint_t **list)
 		else
 			current = current->next;
 	}
+}
+/**
+ * len_list - returns the length of a linked list
+ * @h: pointer to the list
+ *
+ * Return: length of list
+ */
+int len_list(listint_t *h)
+{
+	int len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
 }
