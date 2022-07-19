ffcb36dc021b149bf8de2a2799b3fc6596a2ff9b
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-07-19 22:36:00 +0300

    Heap Sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -11,7 +11,7 @@ void heap_sort(int *array, size_t size)
 {
 	int sizet, h, k;
 
-	if (array = NULL || (int)size < 2)
+	if (array == NULL || (int)size < 2)
 		return;
 	sizet = (int)size;
 	for (h = sizet / 2; h >= 0; h--)
